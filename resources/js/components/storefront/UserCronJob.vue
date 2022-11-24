8a49691a19a463ec2b13f3d3a1d1e8b84d5ea397
Chu <cvhainb@gmail.com>
2022-11-24 11:27:04 +0700

    updated

--- resources/js/components/storefront/UserCronJob.vue
+++ resources/js/components/storefront/UserCronJob.vue
@@ -3,7 +3,31 @@
         <div class="row justify-content-center">
             <div class="col-12 d-flex justify-content-between align-items-center mb-3">
                 <h5 class="mb-3">Cron jobs</h5>
-                <button class="btn btn-primary">Add new cron job</button>
+
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cron-modal">Create a new cron</button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="cron-modal" tabindex="-1" aria-labelledby="cron-modal-label" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <form @submit.prevent="createCron">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="cron-modal-label">Cron</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="col-12">
                 <div class="card card-body">
@@ -15,17 +39,20 @@
 </template>
 
 <script>
+import Modal from 'bootstrap/js/dist/modal';
 import { mapGetters, mapState } from 'vuex'
 export default {
     data: () => ({
         formdata: {
         }
     }),
-    created() {
-
+    mounted() {
+        new Modal(document.getElementById('cron-modal'))
     },
     methods: {
-        
+        createCron() {
+            alert(1)
+        }
     },
     computed: {
         ...mapGetters(['urlGetAllParams', 'urlParamValueFromName']),
