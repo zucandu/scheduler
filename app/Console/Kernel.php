68bdf5a030b12f284a51c8275d9001e1f33bac42
Chu <cvhainb@gmail.com>
2022-11-24 21:17:25 +0700

    setting schedule on server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Storage;
 use Http;
+use Log;
 use DB;
 
 class Kernel extends ConsoleKernel
@@ -28,59 +29,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            
-			$orders = DB::table('orders')->whereNull('tracking_number')->get();
-			foreach($orders as $order) {
-				
-				$user = DB::table('users')->where('id', $order->user_id)->first();
-				
-				// Shipstation key/secret
-				$apiKey = $user->api_key;
-				$apiSecret = $user->api_secret;
-
-				$response = Http::withHeaders([
-					'Content-Type' => 'application/json',
-					'Authorization' => 'Basic ' . base64_encode($apiKey . ':' . $apiSecret)
-				])->get('https://ssapi.shipstation.com/shipments?orderId=' . $order->shipstation_order_id);
-				
-				$json = $response->json();
-				if(isset($json['shipments']) && count($json['shipments']) > 0) {
-					foreach($json['shipments'] as $shipment) {
-						if(isset($shipment['trackingNumber']) && !empty($shipment['trackingNumber'])) {
-							
-							DB::table('orders')->where([
-								'user_id' => $order->user_id,
-								'shipstation_order_id' => $order->shipstation_order_id
-							])->update(['tracking_number' => $shipment['trackingNumber']]);
-							
-							// Send data to store
-							$token = Storage::disk('local')->get("stores/{$user->store_url}");
-							if(empty($token)) {
-								return response()->json(['message' => 'Token not found!'], 422);
-							}
-							
-							$tmp = json_decode($order->order_data, true);
-							$serviceCode = explode('_', $tmp['serviceCode']);
-							$courier = $serviceCode[0];
-							$trackingNumber = $shipment['trackingNumber'];
-							$orderData = [
-								'order_notes' => [
-									'order_id' => $order->store_order_id,
-									'order_status_id' => 4, //'Shipped'
-									'courier' => $courier,
-									'tracking_number' => $trackingNumber,
-									'note' => "Order #{$order->store_order_id} was shipped by {$courier}. The tracking number is #: {$trackingNumber}."
-								],
-								'notify' => 'email'
-							];
-							$response = Http::withToken($token)->accept('application/json')->post("https://{$user->store_url}/api/v1/app/update-order-status", $orderData);
-							
-						}
-					}
-				}
-			}
-			
-        })->hourly();
+			Log::debug("Hello");
+		})->everyMinute();
     }
 
     /**
