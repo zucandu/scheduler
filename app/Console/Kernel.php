b2db17d3f43c41714811a9b06fcabb15b23cdece
Chu <cvhainb@gmail.com>
2022-12-03 16:04:09 +0700

    updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Carbon\Carbon;
 use Storage;
 use Http;
 use DB;
